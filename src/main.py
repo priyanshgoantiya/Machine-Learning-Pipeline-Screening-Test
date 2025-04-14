676e0796fc336fdb535abb73985565b90fa98389
Priyansh Goantiya <161962313+priyanshgoantiya@users.noreply.github.com>
2025-04-14 23:59:44 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -61,7 +61,7 @@ def load_config(json_file='config/algoparams_from_ui.json'):
     except FileNotFoundError:
         raise FileNotFoundError(f"Configuration file {json_file} not found")
 
-def load_data(config, data_file='data/data.csv'):
+def load_data(config, data_file='data'):
     """Load dataset and split into features and target."""
     try:
         data = pd.read_csv(data_file)
